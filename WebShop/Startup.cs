60262db9c3d0a339917edd743812a8b904ac5474
Stepan <stepanc665@gmail.com>
2022-10-31 18:55:27 +0300

    хаха

--- WebShop/Startup.cs
+++ WebShop/Startup.cs
@@ -20,7 +20,7 @@ namespace WebShop
         {
             Configuration = configuration;
         }
-
+        //хаха
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
